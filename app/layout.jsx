cf8f79ecff6706b7615428b540c275657ff694b5
Gino Ruben Giorgi <ginorubengiorgi@gmail.com>
2025-09-07 18:23:02 -0300

    Inserted components and containers in the page

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,12 +1,7 @@
-import { Navbar } from "../components";
-
 export default function RootLayout({ children }) {
     return (
         <html lang="en">
-            <body>
-                <Navbar />
-                {children}
-            </body>
+            <body>{children}</body>
         </html>
     );
 }
--- app/page.jsx
+++ app/page.jsx
@@ -1,3 +1,27 @@
+import {
+    Footer,
+    Blog,
+    Possibility,
+    Features,
+    WhatGPT5,
+    Header,
+} from "../containers";
+import { Cta, Brand, Navbar } from "../components";
+
 export default function Home() {
-    return <div>CHAT-GPT 3</div>;
+    return (
+        <div className="App">
+            <div className="gradient_bg">
+                <Navbar />
+                <Header />
+            </div>
+            <Brand />
+            <WhatGPT5 />
+            <Features />
+            <Possibility />
+            <Cta />
+            <Blog />
+            <Footer />
+        </div>
+    );
 }
