acd6f537818f7a67bad6a7b046baff296608403f
Gino Ruben Giorgi <ginorubengiorgi@gmail.com>
2025-11-01 19:24:59 -0300

    Created and styled cta

--- components/cta/Cta.jsx
+++ components/cta/Cta.jsx
@@ -1,7 +1,19 @@
 import "./cta.css";
 
 const Cta = () => {
-    return <div>Cta</div>;
+    return (
+        <div className="gpt5__cta">
+            <div className="gpt5__cta-content">
+                <p>Request Early Access to Get Started</p>
+                <h3>
+                    Register today & start exploring the endless possibilities.
+                </h3>
+            </div>
+            <div className="gpt5__cta-btn">
+                <button type="button">Get Started</button>
+            </div>
+        </div>
+    );
 };
 
 export default Cta;
